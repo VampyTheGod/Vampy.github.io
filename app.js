b98457f18d967af41993e2be303f3a1ad28e21dd
VampyTheGod <ecstasybeatsmail@gmail.com>
2016-01-20 15:36:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '5465s4d65f4sa6d5f4as6d5f4s6d5f4as64f_dsfsf444f',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  redirect_uri: 'https://VampyTheGod.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
